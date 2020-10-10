f14d75f2592fd727e6d7626ac61f009e84cfff25
sonicswift sonicswift <bche498@aucklanduni.ac.nz>
2020-10-11 11:44:03 +1300

    added repeated sampling of route generation

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 from functions import *
+from copy import deepcopy
 
 
 if __name__ == "__main__":
@@ -16,39 +17,50 @@ if __name__ == "__main__":
     route_index = pd.Series(data=store_index, index = Locations['Store'])
 
     #Partition nodes into north & south groups with 3 subgroups in each
-    North_part, South_part = partition(Locations)
-    Partitions = North_part.copy()
-    Partitions.extend(South_part)
+    North_part_main, South_part_main = partition(Locations)
+    Partitions = North_part_main.copy()
+    Partitions.extend(South_part_main)
+
     #Generate demand estimates for each node 
     demand_data = generate_demand_estimate(Locations)
+
     #Generate List of routes for partitions
+    no_generations = 100 #Change if we need more
+
 
     #Create route storage for linear progam with both distribution centres
     feasible_routes = []
-    for part in North_part:
-        #Create route from path & add to total set 
-        routes = route_gen(Locations,'Distribution North',part,Durations,demand_data,route_index)
-        for route in routes:
-            feasible_routes.append(route)
+    for i in range(no_generations): 
+        #Deepcopy by value as parition nodes are popped during each cycle
+        North_part = deepcopy(North_part_main)
+        South_part = deepcopy(South_part_main)
+
+        for part in North_part:
+            #Create route from path & add to total set 
+            routes = route_gen(Locations,'Distribution North',part,Durations,demand_data,route_index)
+            for route in routes:
+                feasible_routes.append(route)
      
-    for part in South_part:
-        #Create route from path & add to total set 
-        routes = route_gen(Locations,'Distribution South',part,Durations,demand_data,route_index)
-        for route in routes:
-            feasible_routes.append(route)
+        for part in South_part:
+            #Create route from path & add to total set 
+            routes = route_gen(Locations,'Distribution South',part,Durations,demand_data,route_index)
+            for route in routes:
+                feasible_routes.append(route)
 
 
     #For scenario of closing Northen distribution centre
     feasible_routes_south = []
     North_part, South_part = partition(Locations)
-    Partitions = North_part.copy()
-    Partitions.extend(South_part)
-
-    for part in Partitions:
+    Partitions_main = North_part.copy()
+    Partitions_main.extend(South_part)
+    
+    for i in range(no_generations): 
+        Partitions = deepcopy(Partitions_main)
+        for part in Partitions:
         #Create route from path & add to total set 
-        routes = route_gen(Locations,'Distribution South',part,Durations,demand_data,route_index)
-        for route in routes:
-            feasible_routes_south.append(route)
+            routes = route_gen(Locations,'Distribution South',part,Durations,demand_data,route_index)
+            for route in routes:
+                feasible_routes_south.append(route)
 
     print()
 
